c4030c5601f49ef4d4b6f5bf7c8b2c6d082b43fb
Panke <pank51591@foxmail.com>
2023-10-19 17:18:00 +0800

    添加内容

--- STM32_Project_DEMO_20230315/USB—外部FLASH模拟U盘/Mass_Storage/src/hw_config.c
+++ STM32_Project_DEMO_20230315/USB—外部FLASH模拟U盘/Mass_Storage/src/hw_config.c
@@ -130,7 +130,7 @@ void USB_Interrupts_Config(void)
   NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
 
 
-  NVIC_InitStructure.NVIC_IRQChannel = USB_LP_CAN1_RX0_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannel = USB_LP_CAN1_RX0_IRQn;     //÷–∂œœÚ¡ø
   NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
---
+++ 学习例程/STM32F103-USB-CDC-CMSIS-master/Objects/usblib.d
@@ -0,0 +1,12 @@
+.\objects\usblib.o: Src\usblib.c
+.\objects\usblib.o: .\Inc\usblib.h
+.\objects\usblib.o: D:\Program Files (x86)\keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\usblib.o: D:\Program Files (x86)\keil_v5\Keil\STM32F1xx_DFP\2.4.0\Device\Include\stm32f10x.h
+.\objects\usblib.o: .\RTE\_STM32F103-USB-CDC-CMSIS\RTE_Components.h
+.\objects\usblib.o: D:\Program Files (x86)\keil_v5\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h
+.\objects\usblib.o: D:\Program Files (x86)\keil_v5\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\usblib.o: D:\Program Files (x86)\keil_v5\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\usblib.o: D:\Program Files (x86)\keil_v5\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\usblib.o: D:\Program Files (x86)\keil_v5\Keil\STM32F1xx_DFP\2.4.0\Device\Include\system_stm32f10x.h
+.\objects\usblib.o: D:\Program Files (x86)\keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\objects\usblib.o: D:\Program Files (x86)\keil_v5\ARM\ARMCC\Bin\..\include\string.h
